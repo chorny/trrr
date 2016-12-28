628572e4aa8c97aad0134df53cc8a179c0e5f4e5
Zdenek <zed448@icloud.com>
2016-12-28 19:00:34 +0100

    HTTP::Tinyish dependency

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -11,51 +11,35 @@ App::Trrr::TPB - The PirateBay API
 our $VERSION = '0.01';
 
 use strict;
-
-use HTTP::Tiny;
-use Data::Dumper;
+use Carp;
+use HTTP::Tinyish;
 
 sub tpb {
     my @keywords = @_;
+    my $url = 'https://thepiratebay.org/search/' . join('%20', @keywords) . '/0/99/0';
 
-    my $response = HTTP::Tiny->new->get('https://thepiratebay.org/search/' . join('%20', @keywords));
-    die "Failed!\n" unless $response->{success};
+    my $response = HTTP::Tinyish->new->get( $url );
+    croak "Failed to get $url\n" unless $response->{success};
+    print $response->{content};
+    print "\n--------";
      
     my @item = ();
-    open(my $fh,'<',\$response->{content}) || die "cant open response $!";
+    open(my $fh,"<",\$response->{content}) || die "cant open response $!";
+    my %t = ();
     while(<$fh>){
-        my %item = ();
-        if(/detName/){ 
-            $item{title} = $_;
-            $item{title} =~ s/(.*title.*?\>)(.*?)(\<\/a)/$2/;
-            $item{title} = $2;
-        }
-        if(/\<a href\=\"magnet/){
-            $item{magnet} = $_;
-            $item{magnet} =~ s/(\<a href\=\")(magnet.*?)(\".*)/$2/;
-            $item{magnet} = $2
-        }
-        if(/Size.*?\ /){ 
-            $item{size} = $_;
-            $item{size} =~ s/(.*?)(Size.*?\ )(.*?)(\&nbsp\;)(...)/$3$5/;
-            $item{size} = $3 . $5;
-        }
+        if(/detName/){ $t{title} = $_; $t{title} =~ s/(.*?title\=\"Details for )(.*?)(\".*)/$2/ }
+        if(/\<a href\=\"magnet/){ $t{magnet} = $_;$t{magnet} =~ s/(\<a href\=\")(magnet.*?)(\".*)/$2/ }
+        if(/Size.*?\ /){ $t{size} = $_;$t{size} =~ s/(.*?)(Size.*?\ )(.*?)(\&nbsp\;)(...)/$3$5/ }
         if(/More from this category/){
-            $item{category} = $_;
-            $item{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-            $item{category} = $2;
-        }
-        if(defined $item{magnet} and defined $item{title} and defined $item{size} and defined $item{category}){ 
-            push @item, {%item};
-            my %item = ();
+            $t{category} = $_;$t{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
+            chomp($t{magnet}, $t{title}, $t{size}, $t{category});
+            push @item, {%t};
             next;
-        } 
+        }
     }
     return \@item;
 }
 
-#my @r = @{tpb(@ARGV)};
-#print Dumper @r;
 1;
 
 __DATA__

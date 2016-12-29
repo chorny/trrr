c6aedd9d1ea367ed00734654fd9bbff66767f30a
Bohuněk Zdeněk <zbohunek@trask.cz>
2016-12-29 14:51:54 +0100

    double TPB results fix

--- lib/App/Trrr/TPB.pm
+++ lib/App/Trrr/TPB.pm
@@ -21,24 +21,28 @@ sub tpb {
     my $response = HTTP::Tinyish->new->get( $url );
     croak "Failed to get $url\n" unless $response->{success};
      
-    my( @item, %t, $leechs ) = ();
+    my $category = 0;
+    my( @item, %t, $leechs, ) = ();
     open(my $fh,"<",\$response->{content}) || die "cant open response $!";
     while(<$fh>){
-        if(/detName/){ $t{title} = $_; $t{title} =~ s/(.*?title\=\"Details for )(.*?)(\".*)/$2/ }
+        # if(/detName/){ 
+            $t{title} = $_ and $t{title} =~ s/(.*?title\=\"Details for )(.*?)(\".*)/$2/ if /detName/;
+            #$t{title} = $_; $t{title} =~ s/(.*?title\=\"Details for )(.*?)(\".*)/$2/ 
+            #}
         if(/\<a href\=\"magnet/){ $t{magnet} = $_;$t{magnet} =~ s/(\<a href\=\")(magnet.*?)(\".*)/$2/ }
         if(/Size.*?\ /){    $t{size} = $_;$t{size} =~ s/(.*?)(Size.*?\ )(.*?)(\&nbsp\;)(...)(.*)/$3$5/ }
         if(/<td align="right">/){  
             unless($leechs){
                 $t{seeds} = $_; $t{seeds} =~ s/(.*?<td align="right">)([0-9]+)(<.*)/$2/; $leechs = 1;
             } else { $t{leechs} = $_; $t{leechs} =~ s/(.*?<td align="right">)([0-9]+)(<.*)/$2/; $leechs = 0 }
-
-
         }
         if(/More from this category/){
-            $t{category} = $_;$t{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
-            chomp($t{magnet}, $t{title}, $t{size}, $t{category}, $t{seeds}, $t{leechs});
-            push @item, {%t};
-            next;
+            if($category == 0){
+                $t{category} = $_;$t{category} =~ s/(.*category\"\>)(.*?)(\<.*)/$2/;
+                chomp($t{magnet}, $t{title}, $t{size}, $t{category}, $t{seeds}, $t{leechs});
+                push @item, {%t};
+                $category = 1;
+            } else { $category = 0 }
         }
     }
     return \@item;

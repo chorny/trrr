27e3cab710a17f211bd08b42f81d9d68ff29b61f
z448 <zed448@icloud.com>
2016-12-29 22:47:29 +0100

    Changes file updated

--- lib/App/Trrr/KAT.pm
+++ lib/App/Trrr/KAT.pm
@@ -10,7 +10,7 @@ App::Trrr::KAT - KickAss API
 @EXPORT_OK = qw( kat );
 our $VERSION = '0.01';
 
-use 5.010;
+use warnings;
 use strict;
 use URI::Encode qw(uri_decode);
 use Carp;
@@ -20,30 +20,24 @@ sub kat {
     my @keywords = @_;
     my $url = 'http://kickasstorrents.to/usearch/' . join('%20', @keywords) . '/';
 
-    my $response = HTTP::Tiny->new->get( $url );
-    croak "Failed to get $url\n" unless $response->{success};
+    my $response;
+    if(`which curl`){ 
+        $response->{content} = `curl -skL '$url'`;
+    } else {
+        $response = HTTP::Tiny->new->get( $url );
+        croak "Failed to get $url\n" unless $response->{success};
+    }
      
     my( @item, %t ) = ();
     open(my $fh,"<",\$response->{content}) || die "cant open response $!";
     while(<$fh>){
-        if(/data-sc-params="\{ 'name'\:/){
-            s/(.*\{ 'name': ')(.*?)(\'.*)(magnet\:.*?)('.*)/$2$4/;
-            $t{title} = $2; $t{magnet} = $4;
-            $t{title} = uri_decode($t{title});
-        }
-        if(/<td class="nobr center">/){
-            $t{size} = $_;
-            $t{size} =~ s/(<td class="nobr center">)(.*?)( <span>)(.)(.*)/$2$4/;
-            #$t{size} =~ s/(<td class="nobr center">)(.*?)( <span>)(.*?)(<.*)/$2$4/;
-        }
-        if(/<td class="green center">/){
-            $t{seeds} = $_;
-            $t{seeds} =~ s/(<td class="green center">)(.*?)(\<.*)/$2/;
-        }
+        s/(.*\{ 'name': ')(.*?)(\'.*)(magnet\:.*?)('.*)/$2$4/ and $t{title} = uri_decode($2) and $t{magnet} = $4 if /data-sc-params="\{ 'name'\:/;
+        $t{size} = $_ and $t{size} =~ s/(<td class="nobr center">)(.*?)( <span>)(.)(.*)/$2$4/ if /<td class="nobr center">/;
+        $t{category} = $_ and $t{category} =~ s/(.*?span id="cat_.*?href=".*?">)(.*?)(<\/a.*)/$2/ if /^Posted/;
+        $t{seeds} = $_ and $t{seeds} =~ s/(<td class="green center">)(.*?)(\<.*)/$2/ if /<td class="green center">/;
+
         if(/<td class="red lasttd center">/){
-            $t{leechs} = $_;
-            $t{leechs} =~ s/(<td class="red lasttd center">)(.*?)(\<.*)/$2/;
-            $t{category} = 'Video';
+            $t{leechs} = $_; $t{leechs} =~ s/(<td class="red lasttd center">)(.*?)(\<.*)/$2/;
             chomp($t{magnet}, $t{title}, $t{size}, $t{category}, $t{seeds}, $t{leechs});
             push @item, {%t};
         }
@@ -51,3 +45,16 @@ sub kat {
     return \@item;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+# $t{title} =$_ and $t{title} =~ s/(.*?class="red">)(.*)(<\/a.*)/$2/ and $t{title} =~ s/<\/strong>// if /cellMainLink/;

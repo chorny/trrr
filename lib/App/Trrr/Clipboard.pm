40be05f6e396c3598bcb4cf4012a3f49fcc92300
Zdenek <z@nuc>
2016-12-31 02:44:06 +0100

    ios clipboard support

--- lib/App/Trrr/Clipboard.pm
+++ lib/App/Trrr/Clipboard.pm
@@ -6,7 +6,6 @@ our $VERSION = '0.03';
 
 use warnings;
 use strict;
-#use Mac::PropertyList;
 
 sub os {
     open my $pipe,"-|",'uname -a';
@@ -30,19 +29,40 @@ sub tool {
 sub clip {
     my $tool = tool();
     my $clip = sub {
-        #my $clip = {
-        #linux =>  sub{ 
-            #linux =>  sub{ 
             my $string = shift || undef;
             unless( $string ){ my $read = `$tool`; chomp($read); return $read }
             else { return system("echo $string | $tool") }
-            #},
     };
     return $clip;
-    #return $clip->{$os};
 };
 
 
+sub ios_clip {
+       my $data;
+       my $c = '/private/var/mobile/Library/Caches/com.apple.UIKit.pboard/pasteboardDB';
+       local $/;
+       open(my $fh,"<",$c) || die "cant open $c: $!";
+       $data = <$fh>; close $fh;
+       my $load = eval {
+           require Mac::PropertyList;
+           Mac::PropertyList->import('parse_plist');1;
+       };
+
+       unless($load){ return }
+       else {
+           my $plist = Mac::PropertyList::parse_plist( $data );
+           for(@{$plist}){
+               my $s = $_->as_perl;
+               unless($s eq 1){          
+                   if($s->{name} eq 'com.apple.UIKit.pboard.general'){
+                       for(@{$s->{items}->{mobile}}){ return $_->{'public.utf8-plain-text'} }
+                   }
+               }
+           }
+       }
+}
+
+1;
 
 
 
